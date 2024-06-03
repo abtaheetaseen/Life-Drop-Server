01e9c9810ba5eb41f752474822ee77aff84bcb48
abtaheetaseen <abtahee.siddiqui@northsouth.edu>
2024-06-03 10:59:52 +0600

    eleventh commit

--- index.js
+++ index.js
@@ -203,6 +203,13 @@ async function run() {
         res.send(result);
     })
 
+    app.delete("/donationRequest/:id", async(req, res) => {
+        const id = req.params.id;
+        const query = { _id : new ObjectId(id) };
+        const result = await donationRequestCollection.deleteOne(query);
+        res.send(result);
+    })
+
     // get all divisions
     app.get("/divisions", async(req, res) => {
         const result = await divisionCollection.find().toArray();
